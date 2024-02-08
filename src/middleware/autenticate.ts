df35b016f8cec155172566b88d8977f0c4528a66
edchavezb <edgp.chavez@gmail.com>
2024-02-07 18:26:28 -0600

    Remove reference to Mongoode model

--- src/middleware/autenticate.ts
+++ src/middleware/autenticate.ts
@@ -1,31 +1,30 @@
 import { NextFunction, Response, Request } from "express";
 import firebaseAdmin from "../lib/firebase";
-import { UserModel } from "../models/user";
 
 export default async function (req: Request, res: Response, next: NextFunction) {
-  try {
-    const firebaseToken = req.headers.authorization?.split(" ")[1];
+  // try {
+  //   const firebaseToken = req.headers.authorization?.split(" ")[1];
 
-    let firebaseUser;
-    if (firebaseToken) {
-      firebaseUser = await firebaseAdmin.auth.verifyIdToken(firebaseToken);
-    }
+  //   let firebaseUser;
+  //   if (firebaseToken) {
+  //     firebaseUser = await firebaseAdmin.auth.verifyIdToken(firebaseToken);
+  //   }
 
-    if (!firebaseUser) {
-      // Unauthorized
-      return res.sendStatus(401);
-    }
+  //   if (!firebaseUser) {
+  //     // Unauthorized
+  //     return res.sendStatus(401);
+  //   }
 
-    const user = await UserModel.findOne({
-      firebaseId: firebaseUser.user_id
-    });
+  //   const user = await UserModel.findOne({
+  //     firebaseId: firebaseUser.user_id
+  //   });
 
-    req.user = user || undefined;
+  //   req.user = user || undefined;
 
-    next();
-  } catch (err) {
-    console.log(err)
-    //Unauthorized
-    res.sendStatus(401);
-  }
+  //   next();
+  // } catch (err) {
+  //   console.log(err)
+  //   //Unauthorized
+  //   res.sendStatus(401);
+  // }
 }

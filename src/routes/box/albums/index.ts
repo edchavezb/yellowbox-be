8f9913e75201e5d84c7571e5c594d060c620fb0c
edchavezb <edgp.chavez@gmail.com>
2024-03-05 13:39:20 -0600

    298: Add endpoints for tracks, albums, artists and playlists

--- src/routes/box/albums/index.ts
+++ src/routes/box/albums/index.ts
@@ -1,5 +1,172 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const routes = Router();
+
+// Add an album to a box
+routes.post("/:boxId/albums", async (req, res) => {
+  try {
+    const { boxId } = req.params;
+    const { spotifyId, spotifyUrl, spotifyUri, name, images, type, albumType, artists, releaseDate, totalTracks, boxPosition } = req.body;
+
+    // Check if the artist already exists. If not create it.
+    const album = await prisma.album.upsert({
+      where: { spotifyId: spotifyId },
+      update: {},
+      create: {
+        spotifyUrl,
+        spotifyId,
+        spotifyUri,
+        name,
+        images,
+        type,
+        albumType,
+        artists,
+        releaseDate,
+        totalTracks
+      }
+    });
+
+    const newBoxAlbum = await prisma.boxAlbum.create({
+      data: {
+        position: boxPosition,
+        note: "",
+        box: { connect: { boxId: boxId } },
+        album: { connect: { albumId: album.albumId } }
+      }
+    });
+
+    return res.status(201).json(newBoxAlbum);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+// Reorder an album in a box
+routes.put("/:boxId/reorderAlbum/:albumId", async (req, res) => {
+  try {
+    const { boxId, albumId } = req.params;
+    const newPosition = parseInt(req.body.position);
+
+    const targetAlbum = await prisma.boxAlbum.findUnique({
+      where: { albumId_boxId: { boxId: boxId, albumId: albumId } },
+      select: { position: true }
+    });
+
+    if (!targetAlbum) {
+      return res.status(404).json({ error: "Album not found" });
+    }
+
+    await prisma.boxAlbum.update({
+      where: { albumId_boxId: { boxId: boxId, albumId: albumId } },
+      data: { position: newPosition }
+    });
+
+    await prisma.boxAlbum.updateMany({
+      where: {
+        boxId: boxId,
+        albumId: { not: albumId },
+        position: { gte: newPosition }
+      },
+      data: { position: { increment: 1 } }
+    });
+
+    return res.status(200).json({ message: "Album reordered successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+// Update an album's images
+routes.put(":albumId/images", async (req, res) => {
+  try {
+    const { albumId } = req.params;
+    const { images } = req.body;
+
+    const updatedAlbum = await prisma.album.update({
+      where: {
+        albumId: albumId,
+      },
+      data: {
+        images: images
+      },
+    });
+
+    return res.status(200).json({ updatedAlbum });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+// Delete an album from a box
+routes.delete("/:boxId/albums/:albumId", async (req, res) => {
+  try {
+    const { boxId, albumId } = req.params;
+
+    const boxAlbumCount = await prisma.boxAlbum.count({
+      where: { albumId: albumId }
+    });
+
+    await prisma.boxAlbum.delete({
+      where: { albumId_boxId: { boxId: boxId, albumId: albumId } },
+    });
+
+    if (boxAlbumCount === 1) {
+      await prisma.album.delete({
+        where: { albumId: albumId }
+      });
+
+      return res.status(200).json({ message: "Album and its associated BoxAlbum deleted successfully" });
+    } else {
+      return res.status(200).json({ message: "Only the associated BoxAlbum deleted. Album was not deleted." });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+// Add an album to a subsection
+routes.post(":subsectionId/albums/:albumId", async (req, res) => {
+  try {
+    const { albumId, subsectionId, subsectionPosition } = req.params;
+
+    // Create a record in the BoxSubsectionAlbum table
+    await prisma.boxSubsectionAlbum.create({
+      data: {
+        position: parseInt(subsectionPosition),
+        note: "",
+        album: { connect: { albumId: albumId } },
+        boxSubsection: { connect: { subsectionId: parseInt(subsectionId) } }
+      }
+    });
+
+    return res.status(201).json({ message: "Album added to subsection successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+// Remove an album from a subsection
+routes.delete(":subsectionId/albums/:albumId", async (req, res) => {
+  try {
+    const { albumId, subsectionId } = req.params;
+
+    // Delete the record from the BoxSubsectionAlbum table
+    await prisma.boxSubsectionAlbum.delete({
+      where: { albumId_boxSubsectionId: { albumId, boxSubsectionId: parseInt(subsectionId) } },
+    });
+
+    return res.status(200).json({ message: "Album removed from subsection successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
 
 export default routes;
--- src/routes/box/playlists/index.ts
+++ src/routes/box/playlists/index.ts
@@ -1,5 +1,172 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const routes = Router();
+
+// Add a playlist to a box
+routes.post("/:boxId/playlists", async (req, res) => {
+  try {
+    const { boxId } = req.params;
+    const { spotifyId, spotifyUrl, spotifyUri, name, description, ownerName, ownerUri, images, type, totalTracks, boxPosition } = req.body;
+
+    // Check if the playlist already exists. If not create it.
+    const playlist = await prisma.playlist.upsert({
+      where: { spotifyId: spotifyId },
+      update: {},
+      create: {
+        spotifyUrl,
+        spotifyId,
+        spotifyUri,
+        name,
+        description,
+        ownerName,
+        ownerUri,
+        images,
+        type,
+        totalTracks
+      }
+    });
+
+    const newBoxPlaylist = await prisma.boxPlaylist.create({
+      data: {
+        position: boxPosition,
+        note: "",
+        box: { connect: { boxId: boxId } },
+        playlist: { connect: { playlistId: playlist.playlistId } }
+      }
+    });
+
+    return res.status(201).json(newBoxPlaylist);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+// Reorder a playlist in a box
+routes.put("/:boxId/reorderPlaylist/:playlistId", async (req, res) => {
+  try {
+    const { boxId, playlistId } = req.params;
+    const newPosition = parseInt(req.body.position);
+
+    const targetPlaylist = await prisma.boxPlaylist.findUnique({
+      where: { playlistId_boxId: { boxId: boxId, playlistId: playlistId } },
+      select: { position: true }
+    });
+
+    if (!targetPlaylist) {
+      return res.status(404).json({ error: "Playlist not found" });
+    }
+
+    await prisma.boxPlaylist.update({
+      where: { playlistId_boxId: { boxId: boxId, playlistId: playlistId } },
+      data: { position: newPosition }
+    });
+
+    await prisma.boxPlaylist.updateMany({
+      where: {
+        boxId: boxId,
+        playlistId: { not: playlistId },
+        position: { gte: newPosition }
+      },
+      data: { position: { increment: 1 } }
+    });
+
+    return res.status(200).json({ message: "Playlist reordered successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+// Update a playlist's images
+routes.put(":playlistId/images", async (req, res) => {
+  try {
+    const { playlistId } = req.params;
+    const { images } = req.body;
+
+    const updatedPlaylist = await prisma.playlist.update({
+      where: {
+        playlistId: playlistId,
+      },
+      data: {
+        images
+      },
+    });
+
+    return res.status(200).json({ updatedPlaylist });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+// Delete a playlist from a box
+routes.delete("/:boxId/playlists/:playlistId", async (req, res) => {
+  try {
+    const { boxId, playlistId } = req.params;
+
+    const boxPlaylistCount = await prisma.boxPlaylist.count({
+      where: { playlistId: playlistId }
+    });
+
+    await prisma.boxPlaylist.delete({
+      where: { playlistId_boxId: { boxId: boxId, playlistId: playlistId } },
+    });
+
+    if (boxPlaylistCount === 1) {
+      await prisma.playlist.delete({
+        where: { playlistId: playlistId }
+      });
+
+      return res.status(200).json({ message: "Playlist and its associated BoxPlaylist deleted successfully" });
+    } else {
+      return res.status(200).json({ message: "Only the associated BoxPlaylist deleted. Playlist was not deleted." });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+// Add a track to a subsection
+routes.post(":subsectionId/playlists/:playlistId", async (req, res) => {
+  try {
+    const { playlistId, subsectionId, subsectionPosition } = req.params;
+
+    // Create a record in the BoxSubsectionTrack table
+    await prisma.boxSubsectionPlaylist.create({
+      data: {
+        position: parseInt(subsectionPosition),
+        note: "",
+        playlist: { connect: { playlistId: playlistId } },
+        boxSubsection: { connect: { subsectionId: parseInt(subsectionId) } }
+      }
+    });
+
+    return res.status(201).json({ message: "Track added to subsection successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+// Remove a track from a subsection
+routes.delete(":subsectionId/playlists/:playlistId", async (req, res) => {
+  try {
+    const { playlistId, subsectionId } = req.params;
+
+    // Delete the record from the BoxSubsectionTrack table
+    await prisma.boxSubsectionPlaylist.delete({
+      where: { playlistId_boxSubsectionId: { boxSubsectionId: parseInt(subsectionId), playlistId: playlistId } },
+    });
+
+    return res.status(200).json({ message: "Track removed from subsection successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
 
 export default routes;

9c0bde94209899cee62cb3a99a4f38d8e8817e81
edgarchavez <edchavez821@gmail.com>
2025-08-24 22:24:24 -0600

    424: New user page layout supporting endpoints

--- src/services/user/userService.ts
+++ src/services/user/userService.ts
@@ -6,12 +6,12 @@ const userService = {
   async getMyUserData(userId: string) {
     const user = await prisma.user.findFirst({
       where: {
-        userId
+        userId,
       },
       include: {
         boxes: {
           where: {
-            deletedAt: null
+            deletedAt: null,
           },
           select: {
             boxId: true,
@@ -19,11 +19,11 @@ const userService = {
             position: true,
             folderPosition: true,
             folderId: true,
-            isPublic: true
+            isPublic: true,
           },
           orderBy: {
-            position: 'asc'
-          }
+            position: "asc",
+          },
         },
         folders: {
           select: {
@@ -34,380 +34,452 @@ const userService = {
             description: true,
             boxes: {
               where: {
-                deletedAt: null
+                deletedAt: null,
               },
               select: {
                 boxId: true,
                 name: true,
                 position: true,
                 folderPosition: true,
-                isPublic: true
+                isPublic: true,
               },
               orderBy: {
-                folderPosition: 'asc'
-              }
-            }
-          }
+                folderPosition: "asc",
+              },
+            },
+          },
         },
-        billing: true,
-        accountData: true,
-        spotifyAccount: true
-      }
-    });
-
-    return user;
-  },
-  getMyFollowedItems: async (userId: string) => {
-    const user = await prisma.user.findFirst({
-      where: {
-        userId
-      },
-      select: {
-        userId: true,
-        username: false, 
-        imageUrl: false,
-        firstName: false,
-        lastName: false,
         followedUsers: {
           select: {
-            createdAt: true,
             followedUser: {
               select: {
                 userId: true,
                 username: true,
                 imageUrl: true,
                 firstName: true,
-                lastName: true
-              }
-            }
-          }
+                lastName: true,
+              },
+            },
+          },
         },
         followers: {
           select: {
-            createdAt: true,
             follower: {
               select: {
                 userId: true,
                 username: true,
                 imageUrl: true,
                 firstName: true,
-                lastName: true
-              }
-            }
-          }
+                lastName: true,
+              },
+            },
+          },
         },
+        billing: true,
+        accountData: true,
+        spotifyAccount: true,
+      },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    const followedUsers = user.followedUsers.map((item) => item.followedUser);
+    const followers = user.followers.map((item) => item.follower);
+
+    return {
+      ...user,
+      followedUsers,
+      followers,
+    };
+  },
+  async getMyFollowedUsers (userId: string) {
+    const user = await prisma.user.findFirst({
+      where: {
+        userId,
+      },
+      select: {
+        followedUsers: {
+          select: {
+            followedUser: {
+              select: {
+                userId: true,
+                username: true,
+                imageUrl: true,
+                firstName: true,
+                lastName: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!user) {
+      return [];
+    }
+
+    const followedUsers = user.followedUsers.map((item) => item.followedUser);
+
+    return followedUsers;
+  },
+  async getMyFollowedBoxes(userId: string) {
+    const followedBoxes = await prisma.user.findFirst({
+      where: { userId },
+      select: {
         followedBoxes: {
           select: {
-            createdAt: true,
             box: {
               select: {
                 boxId: true,
                 name: true,
-                isPublic: true,
-                folderId: true,
-                folderPosition: true,
                 position: true,
+                folderPosition: true,
+                folderId: true,
+                isPublic: true,
                 creator: {
                   select: {
                     userId: true,
                     username: true,
+                    imageUrl: true,
+                    firstName: true,
+                    lastName: true,
                   },
                 },
-              }
-            }
-          }
+              },
+            },
+          },
+          orderBy: {
+            createdAt: "desc",
+          },
         },
-      }
+      },
     });
-    return user;
+
+    if (!followedBoxes) {
+      return [];
+    }
+    return followedBoxes.followedBoxes.map((item) => item.box);
   },
   async getUserDataById(userId: string) {
-    const user = await prisma.user.findUnique({
-      where: {
-        userId,
-      },
-      include: {
-        folders: {
-          select: {
-            folderId: true,
-            name: true,
-            isPublic: true,
-            creator: true,
-            description: true,
+      const user = await prisma.user.findUnique({
+        where: {
+          userId,
+        },
+        include: {
+          folders: {
+            select: {
+              folderId: true,
+              name: true,
+              isPublic: true,
+              creator: true,
+              description: true,
+            },
+          },
+          boxes: {
+            where: {
+              deletedAt: null,
+            },
+            select: {
+              boxId: true,
+              name: true,
+              position: true,
+              folderPosition: true,
+              folderId: true,
+              isPublic: true,
+            },
+            orderBy: {
+              position: "asc",
+            },
           },
         },
-        boxes: {
-          where: {
-            deletedAt: null,
-          },
-          select: {
-            boxId: true,
-            name: true,
-            position: true,
-            folderPosition: true,
-            folderId: true,
-            isPublic: true,
-          },
-          orderBy: {
-            position: "asc",
-          },
-        },
-      },
-    });
+      });
 
-    return user;
-  },
+      return user;
+    },
   async getUserDataByUsername(username: string) {
-    const user = await prisma.user.findFirst({
-      where: {
-        username,
-      },
-      include: {
-        folders: {
-          select: {
-            folderId: true,
-            name: true,
-            isPublic: true,
-            creator: true,
-            description: true,
+      const user = await prisma.user.findFirst({
+        where: {
+          username,
+        },
+        include: {
+          folders: {
+            select: {
+              folderId: true,
+              name: true,
+              isPublic: true,
+              creator: true,
+              description: true,
+            },
+          },
+          boxes: {
+            where: {
+              deletedAt: null,
+            },
+            select: {
+              boxId: true,
+              name: true,
+              position: true,
+              folderPosition: true,
+              folderId: true,
+              isPublic: true,
+            },
+            orderBy: {
+              position: "asc",
+            },
+          },
+          followedUsers: {
+            select: {
+              createdAt: true,
+              followedUser: {
+                select: {
+                  userId: true,
+                  username: true,
+                  imageUrl: true,
+                  firstName: true,
+                  lastName: true,
+                },
+              },
+            },
+          },
+          followers: {
+            select: {
+              createdAt: true,
+              follower: {
+                select: {
+                  userId: true,
+                  username: true,
+                  imageUrl: true,
+                  firstName: true,
+                  lastName: true,
+                },
+              },
+            },
           },
         },
-        boxes: {
-          where: {
-            deletedAt: null,
-          },
-          select: {
-            boxId: true,
-            name: true,
-            position: true,
-            folderPosition: true,
-            folderId: true,
-            isPublic: true,
-          },
-          orderBy: {
-            position: "asc",
-          },
-        },
-      },
-    });
+      });
 
-    return user;
-  },
+      const followedUsers = user!.followedUsers.map((item) => item.followedUser);
+      const followers = user!.followers.map((item) => item.follower);
+
+      return { ...user, followedUsers, followers };
+    },
   async getUserWithDashboardBoxes(userId: string) {
-    const user = await prisma.user.findFirst({
-      where: {
-        userId
-      },
-      include: {
-        boxes: {
-          where: {
-            folderId: null,
-            deletedAt: null
-          },
-          select: {
-            boxId: true,
-            name: true
+      const user = await prisma.user.findFirst({
+        where: {
+          userId
+        },
+        include: {
+          boxes: {
+            where: {
+              folderId: null,
+              deletedAt: null
+            },
+            select: {
+              boxId: true,
+              name: true
+            }
           }
         }
-      }
-    });
+      });
 
-    return user;
-  },
+      return user;
+    },
   async getUsernameExists(username: string) {
-    const count = await prisma.user.count({
-      where: {
-        username
-      }
-    });
+      const count = await prisma.user.count({
+        where: {
+          username
+        }
+      });
 
-    return !!count
-  },
+      return !!count
+    },
   async getEmailExists(email: string) {
-    const count = await prisma.user.count({
-      where: {
-        email
-      }
-    });
+      const count = await prisma.user.count({
+        where: {
+          email
+        }
+      });
 
-    return !!count
-  },
+      return !!count
+    },
   async createUser(userData: any) {
-    const { firebaseId, username, firstName, lastName, image, email } = userData;
-    const newUser = await prisma.user.create({
-      data: {
-        firebaseId,
-        username,
-        firstName,
-        lastName,
-        imageUrl: image,
-        email
-      }
-    });
+      const { firebaseId, username, firstName, lastName, image, email } = userData;
+      const newUser = await prisma.user.create({
+        data: {
+          firebaseId,
+          username,
+          firstName,
+          lastName,
+          imageUrl: image,
+          email
+        }
+      });
 
-    // Use the userId to create user billing, user account management data, and user queue
-    await prisma.$transaction([
-      prisma.userBilling.create({
-        data: {
-          userId: newUser.userId
-        }
-      }),
-      prisma.userAccountManagementData.create({
-        data: {
-          userId: newUser.userId,
-        }
-      }),
-      prisma.userQueue.create({
-        data: {
-          userId: newUser.userId,
-        }
-      })
-    ]);
+      // Use the userId to create user billing, user account management data, and user queue
+      await prisma.$transaction([
+        prisma.userBilling.create({
+          data: {
+            userId: newUser.userId
+          }
+        }),
+        prisma.userAccountManagementData.create({
+          data: {
+            userId: newUser.userId,
+          }
+        }),
+        prisma.userQueue.create({
+          data: {
+            userId: newUser.userId,
+          }
+        })
+      ]);
 
-    return newUser;
-  },
+      return newUser;
+    },
   async deleteUser(userId: string) {
-    await prisma.user.delete({
-      where: {
-        userId
-      }
-    });
-  },
+      await prisma.user.delete({
+        where: {
+          userId
+        }
+      });
+    },
   async updateUserProfileInfo(userId: string, updatedInfo: any) {
-    const { firstName, lastName } = updatedInfo;
-    const updatedUser = await prisma.user.update({
-      where: {
-        userId
-      },
-      data: {
-        firstName,
-        lastName
-      }
-    });
+      const { firstName, lastName } = updatedInfo;
+      const updatedUser = await prisma.user.update({
+        where: {
+          userId
+        },
+        data: {
+          firstName,
+          lastName
+        }
+      });
 
-    return updatedUser;
-  },
+      return updatedUser;
+    },
   async updateUserImage(userId: string, imageUrl: string) {
-    const updatedUser = await prisma.user.update({
-      where: {
-        userId
-      },
-      data: {
-        imageUrl
-      }
-    });
+      const updatedUser = await prisma.user.update({
+        where: {
+          userId
+        },
+        data: {
+          imageUrl
+        }
+      });
 
-    return updatedUser;
-  },
+      return updatedUser;
+    },
   async verifyUserEmail(userId: string) {
-    const updatedUserAccountData = await prisma.userAccountManagementData.update({
-      where: {
-        userId: userId
-      },
-      data: {
-        emailVerified: true
-      }
-    });
+      const updatedUserAccountData = await prisma.userAccountManagementData.update({
+        where: {
+          userId: userId
+        },
+        data: {
+          emailVerified: true
+        }
+      });
 
-    return updatedUserAccountData;
-  },
+      return updatedUserAccountData;
+    },
   async toggleTutorialPreference(userId: string) {
-    const user = await prisma.userAccountManagementData.findUnique({
-      where: { userId: userId }
-    });
+      const user = await prisma.userAccountManagementData.findUnique({
+        where: { userId: userId }
+      });
 
-    const updatedUserAccountData = await prisma.userAccountManagementData.update({
-      where: {
-        userId: userId
-      },
-      data: {
-        showTutorial: !user?.showTutorial
-      }
-    });
+      const updatedUserAccountData = await prisma.userAccountManagementData.update({
+        where: {
+          userId: userId
+        },
+        data: {
+          showTutorial: !user?.showTutorial
+        }
+      });
 
-    return updatedUserAccountData;
-  },
+      return updatedUserAccountData;
+    },
   async linkSpotifyAccount(userId: string, spotifyData: any) {
-    const { refreshToken, spotifyId } = spotifyData;
-    const updatedUserSpotifyAccount = await prisma.userSpotifyAccount.upsert({
-      where: {
-        userId
-      },
-      update: {
-        refreshToken,
-        spotifyId,
-      },
-      create: {
-        userId,
-        refreshToken,
-        spotifyId,
-      }
-    });
+      const { refreshToken, spotifyId } = spotifyData;
+      const updatedUserSpotifyAccount = await prisma.userSpotifyAccount.upsert({
+        where: {
+          userId
+        },
+        update: {
+          refreshToken,
+          spotifyId,
+        },
+        create: {
+          userId,
+          refreshToken,
+          spotifyId,
+        }
+      });
 
-    return updatedUserSpotifyAccount;
-  },
+      return updatedUserSpotifyAccount;
+    },
   async unlinkSpotifyAccount(userId: string) {
-    await prisma.userSpotifyAccount.delete({
-      where: {
-        userId: userId
-      }
-    });
-  },
+      await prisma.userSpotifyAccount.delete({
+        where: {
+          userId: userId
+        }
+      });
+    },
   async isUserFollowed(followerId: string, followedUserId: string) {
-    const followRecord = await prisma.userFollow.findFirst({
-      where: {
-        followerId,
-        followedUserId,
-      },
-    });
+      const followRecord = await prisma.userFollow.findFirst({
+        where: {
+          followerId,
+          followedUserId,
+        },
+      });
 
-    return !!followRecord;
-  },
+      return !!followRecord;
+    },
   async followUser(followerId: string, followedUserId: string) {
-    if (followerId === followedUserId) {
-      throw new Error("You cannot follow yourself.");
-    }
+      if (followerId === followedUserId) {
+        throw new Error("You cannot follow yourself.");
+      }
 
-    const followRecord = await prisma.userFollow.findFirst({
-      where: {
-        followerId,
-        followedUserId,
-      },
-    });
+      const followRecord = await prisma.userFollow.findFirst({
+        where: {
+          followerId,
+          followedUserId,
+        },
+      });
 
-    if (followRecord) {
-      throw new Error("You are already following this user.");
-    }
+      if (followRecord) {
+        throw new Error("You are already following this user.");
+      }
 
-    const newFollow = await prisma.userFollow.create({
-      data: {
-        followerId,
-        followedUserId,
-      },
-    });
+      const newFollow = await prisma.userFollow.create({
+        data: {
+          followerId,
+          followedUserId,
+        },
+      });
 
-    return newFollow;
-  },
+      return newFollow;
+    },
   async unfollowUser(followerId: string, followedUserId: string) {
-    const followRecord = await prisma.userFollow.findFirst({
-      where: {
-        followerId,
-        followedUserId,
-      },
-    });
+      const followRecord = await prisma.userFollow.findFirst({
+        where: {
+          followerId,
+          followedUserId,
+        },
+      });
 
-    if (!followRecord) {
-      throw new Error("You are not following this user.");
-    }
+      if (!followRecord) {
+        throw new Error("You are not following this user.");
+      }
 
-    await prisma.userFollow.delete({
-      where: {
-        userFollowId: followRecord.userFollowId,
-      },
-    });
+      await prisma.userFollow.delete({
+        where: {
+          userFollowId: followRecord.userFollowId,
+        },
+      });
 
-    return { message: "Successfully unfollowed the user." };
-  },
-};
+      return { message: "Successfully unfollowed the user." };
+    },
+  };
 
-export default userService;
+  export default userService;
